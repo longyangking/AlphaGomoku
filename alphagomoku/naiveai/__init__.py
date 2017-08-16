f68ce32eec92e854d374729e2cc1700832cd402b
longyangking <longyang_123@yeah.net>
2017-08-16 20:50:32 +0800

    Update Naive Computer

--- alphagomoku/naiveai/__init__.py
+++ alphagomoku/naiveai/__init__.py
@@ -1,2 +1,3 @@
 from . import naivecomputer
 from . import naivecomputer2
+from . import naivecomputer3
--- alphagomoku/naiveai/naivecomputer.py
+++ alphagomoku/naiveai/naivecomputer.py
@@ -5,6 +5,7 @@ class Computer:
     Naive Computer with minimal cost simplified table-form strategy
     '''
     def __init__(self):
+        print("Naive Computer with minimal simplified cost")
         pass
 
     def play(self,chessboardinfo):
@@ -12,56 +13,37 @@ class Computer:
         values = self.__values(chessboardinfo)
         flatpos = np.argmax(values)
         return (int(flatpos/width),flatpos%width)      
+    
+    def __getchess(self,pos,chessboardinfo):
+        x,y = pos
+        (width,height) = chessboardinfo.shape
+        if (x < 0) or (x >= width) or (y < 0) or (y >= height):
+            return -1
+        else:
+            return chessboardinfo[x,y]
 
     def __values(self,chessboardinfo):
         (width,height) = chessboardinfo.shape
         values = np.zeros(chessboardinfo.shape)
-        for i in range(width):
-            for j in range(height):
-                if chessboardinfo[i,j] !=0:
-                    values[i,j] = -1
+        for x in range(width):
+            for y in range(height):
+                if chessboardinfo[x,y] !=0:
+                    values[x,y] = -1
                 else:
                     value = 0
-                    if ((i-4 >= 0) and (j-4 >= 0)):
-                        value += 4*chessboardinfo[i-1,j-1] \
-                            + 3*chessboardinfo[i-2,j-2] \
-                            + 2*chessboardinfo[i-3,j-3] \
-                            + chessboardinfo[i-4,j-4]
-                    if (j-4 >= 0):
-                        value += 4*chessboardinfo[i,j-1] \
-                            + 3*chessboardinfo[i,j-2] \
-                            + 2*chessboardinfo[i,j-3] \
-                            + chessboardinfo[i,j-4]
-                    if ((i+4 < width) and (j-4 >= 0)):
-                        value += 4*chessboardinfo[i+1,j-1] \
-                            + 3*chessboardinfo[i+2,j-2] \
-                            + 2*chessboardinfo[i+3,j-3] \
-                            + chessboardinfo[i+4,j-4]
-                    if (i+4 < width):
-                        value += 4*chessboardinfo[i+1,j] \
-                            + 3*chessboardinfo[i+2,j] \
-                            + 2*chessboardinfo[i+3,j] \
-                            + chessboardinfo[i+4,j]
-                    if ((i+4 < width) and (j+4 <height)):
-                        value += 4*chessboardinfo[i+1,j+1] \
-                            + 3*chessboardinfo[i+2,j+2] \
-                            + 2*chessboardinfo[i+3,j+3] \
-                            + chessboardinfo[i+4,j+4]
-                    if (j+4 < height):
-                        value += 4*chessboardinfo[i,j+1] \
-                            + 3*chessboardinfo[i,j+2] \
-                            + 2*chessboardinfo[i,j+3] \
-                            + chessboardinfo[i,j+4]
-                    if ((i-4 >= 0) and (j+4 < height)):
-                        value += 4*chessboardinfo[i-1,j+1] \
-                            + 3*chessboardinfo[i-2,j+2] \
-                            + 2*chessboardinfo[i-3,j+3] \
-                            + chessboardinfo[i-4,j+4]
-                    if (i-4 >= 0):
-                        value += 4*chessboardinfo[i-1,j] \
-                            + 3*chessboardinfo[i-2,j] \
-                            + 2*chessboardinfo[i-3,j] \
-                            + chessboardinfo[i-4,j]
-                    values[i,j] = value + 0.5*np.exp(-((i-width/2)**2/(width/2)**2 + (j-height/2)**2/(height/2)**2))
+                    for bias in range(-4,0):
+                        valuevertical = 0
+                        valuehorizon = 0
+                        valuecross = 0
+                        for i in range(5):
+                            pos = [x + bias + i, y]
+                            valuevertical += self.__getchess(pos,chessboardinfo)
+                            pos = [x, y + bias +i]
+                            valuehorizon += self.__getchess(pos,chessboardinfo)
+                            pos = [x + bias + i, y + bias + i]
+                            valuecross += self.__getchess(pos,chessboardinfo)
+                        value = np.max([value, valuevertical, valuehorizon, valuecross])
+
+                    values[x,y] = value + 0.1*np.exp(-((x-width/2)**2/(width/2)**2 + (y-height/2)**2/(height/2)**2))
                     
         return values        

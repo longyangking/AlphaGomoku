fa970fd26e8851280b94ef59a5af785555a2aa30
Yang Long <longyang_123@yeah.net>
2018-03-17 15:16:19 +0800

    basic structure

--- alphagomoku/Alphagomoku.py
+++ alphagomoku/Alphagomoku.py
@@ -1,10 +1,38 @@
 import numpy as np 
+import argparse
 
 class Alphagomoku:
     def __init__(self):
+        pass
 
     def restart_train(self):
+        pass
 
-    def continue_train(self,model)
+    def continue_train(self,model):
+        pass
 
     def loadai(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="Play with AI or Train AI")
+    #parser.add_argument("--retrain", type=bool, action='store_true', default=False, help="Re-Train AI")
+    #parser.add_argument("--train", type=bool, action='store_true', default=False, help="Train AI")
+    #parser.add_argument("--verbose", type=bool, action='store_true', default=True, help="Show the process information")
+
+    parser.add_argument("--retrain", action='store_true', default=False, help="Re-Train AI")
+    parser.add_argument("--train",  action='store_true', default=False, help="Train AI")
+    parser.add_argument("--verbose", action='store_true', default=True, help="Show the process information")
+
+    args = parser.parse_args()
+    if args.train:
+        print("Train AI")
+
+    if args.retrain:
+        print("Re-train AI")
+
+    if args.verbose:
+        print("Verbose is on")
--- alphagomoku/deepai/MCTS.py
+++ alphagomoku/deepai/MCTS.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import copy
+from operator import itemgetter
 
 def softmax(x):
     probs = np.exp(x-np.max(x))
@@ -44,12 +45,15 @@ class TreeNode:
     def is_root(self):
         return self._parent is None
 
+
+
 class MCTS:
-    def __init__(self,value_function, c_puct, n_playout):
+    def __init__(self,value_function, c_puct, n_playout, verbose=False):
         self._root = TreeNode(None, 1.0)
         self._value_function = _value_function
         self._c_puct = c_puct
         self._n_playout = n_playout
+        self.verbose = verbose
 
     def _playout(self, state):
         node = self._root
@@ -80,6 +84,31 @@ class MCTS:
         actions, visits = zip(*action_visits)
         actions_probs = softmax(1.0/temperature*np.log(np.array(visits) + eps))
         return actions, action_probs
+
+    def get_move(self, state):
+        for i in range(self._n_playout):
+            _state = copy.deepcopy(state)
+            self._playout(_state)
+        return max(self._root._childern.items(),
+            key=lambda action_node: action_node[1]._n_visits)[0]
+
+    def _evaluate_rollout(self, state, n_round):
+        # TODO Pure playout
+        for i in range(n_round):
+            end, winner = 
+            if end:
+                break
+            action_probs = self._rollout_value_function(state)
+            _action = max(action_probs, key=itemgetter(1))[0]
+            state.play(_action)
+        else:
+            if self.verbose:
+                print("Warning: Rollout reaches round limit")
+        
+        # TODO default player
+
+    def _rollout_value_function(self, state):
+
 
     def update_with_move(self, move):
         if move in self._root._childern:
@@ -133,3 +162,20 @@ class MCTSPlayer:
 
     def __str__(self):
         return "Monte Carlo Tree Search: {player}".format(player=player)
+
+def value_function_check(state):
+    # equivalent probability to rollout
+    pass
+
+class Checkplayer:
+    def __init__(self, c_puct, n_playout):
+        self.mcts = MCTS(value_function_check, c_puct, n_playout)
+
+    def set_player_index(self, player):
+        self.player = player
+    
+    def reset_player(self):
+        self.mcts.update_with_move(-1)
+
+    def get_action(self, state):
+        

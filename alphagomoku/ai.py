f6bf350b6d4657f560293a8100515010137abd6f
Yang Long <longyang_123@yeah.net>
2018-06-28 17:31:09 +0800

    update structure

--- alphagomoku/ai.py
+++ alphagomoku/ai.py
@@ -1,6 +1,17 @@
 from __future__ import absolute_import
+from __future__ import print_function
 
-import numpy as np 
+from keras.models import Model
+from keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, Activation, LeakyReLU, add
+from keras.optimizers import SGD
+from keras import regularizers
+import keras.backend as K
+import tensorflow as tf
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Only error will be shown
+
 import copy
 import Config
 from operator import itemgetter
@@ -11,16 +22,18 @@ def softmax(x):
     return probs
 
 class TreeNode:
+    '''
+    Monte Carlo Tree Node
+    '''
     def __init__(self,parent,prior_p):
         self._parent = parent
-        self._childern = {} # Save childre nodes in Hash data structure
+        self._childern = {} 
         self._n_visits = 0
         self._Q = 0
         self._u = 0
         self._P = prior_p
 
     def expand(self,action_priors):
-        #print(action_priors)
         for action, prob in action_priors:
             if action not in self._childern:
                 self._childern[action] = TreeNode(self,prob)
@@ -50,181 +63,68 @@ class TreeNode:
         return self._parent is None
 
 class MCTS:
-    def __init__(self,value_function, c_puct, n_playout, role, verbose=False):
-        self._root = TreeNode(None, 1.0)
-        self._value_function = value_function
-        self._c_puct = c_puct
-        self._n_playout = n_playout
+    def __init__(self, evaluate_function, c_puct, n_playout, role, verbose=False):
+        self.root = TreeNode(None, 1.0)
+        self.evaluate_function = evaluate_function
+        self.c_puct = c_puct
+        self.n_playout = n_playout
         self.role = role
         self.verbose = verbose
 
-    def _playout(self, chessboard):
-        node = self._root
-        roles = chessboard.get_roles()
-        role_index = [index for (index,role) in zip(range(len(roles)),roles) if role==self.role][0]
+    def search(self, engine):
+        '''
+        Simulate MCTS to search best policy for current status
+        '''
+        eps = 1e-12
+        s_reversal = 0
 
-        while 1:
-            if node.is_leaf():
-                break
-            action, node = node.select(self._c_puct)
-            chessboard.playchess_rec(pos_rec=action,role=roles[role_index])
-            role_index = (role_index + 1)%len(roles)
+        for i in range(self.n_playout):
+            if self.root.is_leaf():
+                # Expand and Evaluate
+                value, action_probs = self.evaluate_function(
+                    engine, 
+                    role=self.role, 
+                    s_reversal=s_reversal
+                    )
+                s_reversal = (s_reversal + 1)%2
 
-        leaf_value, action_probs = self._value_function(chessboard=chessboard, role=roles[role_index], verbose=self.verbose)
-        end, winner =  chessboard.get_status()
+                self.root.expand(zip(*action_probs))
 
-        if not end:
-            node.expand(action_probs)
-        else:
-            # for end state，return the "true" leaf_value
-            if winner == None:  # tie
-                leaf_value = 0.0
+                # Backup
+                self.root.update(value)
             else:
-                leaf_value = (
-                    1.0 if winner == self.role else -1.0
-                )
-        
-        # Update the whole MC tree recursively
-        node.update_recursive(leaf_value)
+                # Select 
+                action, node = self.root.select(self.c_puct)
+                engine.play(action)
+                while not node.is_leaf():
+                    action, node = node.select(self.c_puct)
+                    engine.play(action)
 
-    def get_move_probs(self, chessboard, temperature, eps=1e-10):
-        for i in range(self._n_playout):
-            _chessboard = copy.deepcopy(chessboard)
-            self._playout(_chessboard)
+                # Expand and Evaluate
+                value, action_probs = self.evaluate_function(
+                    engine, 
+                    role=self.role, 
+                    s_reversal=s_reversal
+                    )
+                s_reversal = (s_reversal + 1)%2
 
-        action_visits = [(action, node._n_visits) 
-                        for action, node in self._root._childern.items()]
-        print(action_visits)
-        actions, visits = zip(*action_visits)
-        action_probs = softmax(1.0/temperature*np.log(np.array(visits) + eps))
-        return actions, action_probs
+                node.expand(zip(*action_probs))
+                # Backup
+                node.update_recursive(value)
 
-    def get_move(self, chessboard):
-        for i in range(self._n_playout):
-            _chessboard = copy.deepcopy(chessboard)
-            self._playout(_chessboard)
-        return max(self._root._childern.items(),
-            key=lambda action_node: action_node[1]._n_visits)[0]
-
-    def _evaluate_rollout(self, chessboard, n_round):
-        # TODO Pure playout
-        #for i in range(n_round):
-        #    end, winner = 
-        #    if end:
-        #        break
-        #    action_probs = self._rollout_value_function(chessboard)
-        #    _action = max(action_probs, key=itemgetter(1))[0]
-        #    chessboard.play(_action)
-        #else:
-        #    if self.verbose:
-        #        print("Warning: Rollout reaches round limit")
-        pass
-        # TODO default player
-
-    def _rollout_value_function(self, chessboard):
-        pass
-
-    def update_with_move(self, move):
-        if move in self._root._childern:
-            self._root = self._root._childern[move]
-            self._root._parent = None
-        else:
-            self._root = TreeNode(None, 1.0)
-
-    def __str__(self):
-        return "Monte Carlo Tree Search"
-
-class MCTSPlayer:
-    def __init__(self, value_function, c_puct, n_playout, is_selfplay, role, verbose=False):
-        self.mcts = MCTS(value_function, c_puct, n_playout, role, verbose)
-        self._is_selfplay = is_selfplay
-        self.role = role
-        self.verbose = verbose
-
-    def set_player_index(self, player):
-        self.player = player
-
-    def reset_player(self):
-        self.mcts.update_with_move(-1)
-
-    def play(self, chessboard):
-        move, action_prob = self.get_action(chessboard=chessboard, temperature=1.0)
-        x = int(move/board.shape[0])
-        y = move%board.shape[1]
-        return (x,y)
-
-    def get_action(self, chessboard, temperature, return_prob=0):
-        move_probs = np.zeros(chessboard.get_shape())
-        is_available = chessboard.is_available()
-
-        if is_available:
-            actions, probs = self.mcts.get_move_probs(chessboard, temperature)
-            positions = chessboard.rec2pos(actions)
-            move_probs[positions] = probs
-            if self._is_selfplay:
-                move = np.random.choice(
-                    actions,
-                    p=0.75*probs + 0.25*np.random.dirichlet(0.3*np.ones(len(probs)))
-                )
-                self.mcts.update_with_move(move)
-            else:
-                move = np.random.choice(actions,p=probs)
-                self.mcts.update_with_move(-1)
-
-            if return_prob:
-                return move, move_probs
-            else:
-                return move
-        else:
-            if self.verbose:
-                # TODO need modification
-                print("The board is full")
-
-    def play(self,chessboard):
-        temperature = 1.0
-        action = self.get_action(chessboard=chessboard,temperature=temperature,return_prob=False)
-        return chessboard.rec2pos([action])
-
-    def __str__(self):
-        return "Monte Carlo Tree Search: {player}".format(player=player)
-
-def value_function_check(state):
-    # equivalent probability to rollout
-    pass
-
-class Checkplayer:
-    def __init__(self, c_puct, n_playout):
-        self.mcts = MCTS(value_function_check, c_puct, n_playout)
-
-    def set_player_index(self, player):
-        self.player = player
-    
-    def reset_player(self):
-        self.mcts.update_with_move(-1)
-
-    def get_action(self, state):
-        pass
-
-from __future__ import print_function
-
-from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, Activation, LeakyReLU, add
-from keras.optimizers import SGD
-from keras import regularizers
-import keras.backend as K
-import tensorflow as tf
-
-import numpy as np
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Only error will be shown
+        # Play: Return simulation results
+        actions_visits = [(action, node._n_visits) for action, node in self.root._childern.items()]
+        actions, visits = zip(*actions_visits)
+        probs = softmax(1.0/temperature*np.log(np.array(visits) + eps))
+        return actions, probs
 
 def softmax_cross_entropy_with_logits(y_true, y_pred):
 	loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y_pred)
 	return loss
 
-class AlphaZero:
+class NeuralNetwork:
     def __init__(self,
-        input_size,
+        input_shape,
         hidden_layers,
         learning_rate=1e-4,
         momentum=0.9,
@@ -232,14 +132,14 @@ class AlphaZero:
         verbose=False
         ):
 
-        self.input_size = input_size
+        self.input_shape = input_shape
         self.hidden_layers = hidden_layers
 
         self.learning_rate = learning_rate
         self.l2_const = l2_const
         self.momentum = momentum
 
-        self.output_size = self.input_size[0]*self.input_size[1]
+        self.output_size = self.input_shape[0]*self.input_shape[1]
         self.model = None
 
         self.verbose = verbose
@@ -255,29 +155,11 @@ class AlphaZero:
         value = self._policy_value_block(x)
         action_prob = self._action_prob_block(x)
 
-        self.model = Model(inputs=[main_input], outputs=[value,action_prob])
+        self.model = Model(inputs=[main_input], outputs=[value, action_prob])
         self.model.compile(loss={'value_head': 'mean_squared_error', 'policy_head': softmax_cross_entropy_with_logits},
 			optimizer=SGD(lr=self.learning_rate, momentum=self.momentum),	
 			loss_weights={'value_head': 0.5, 'policy_head': 0.5}	
 			)
-        
-    def value_function(self, chessboard, role, verbose=False):
-        state = chessboard.get_state()
-        state = np.array(state)
-        #print(state.shape)
-        state = state.reshape(1, *self.input_size)
-
-        value, prob_act = self.model.predict(state)
-        _, acts = chessboard.get_availables()
-        action_probs = zip(acts, prob_act[0][acts])
-        return value[0][0], action_probs
-
-    def train(self, X, y, batchsize=128, epochs=30, validation_split=0.1, verbose=False):
-        self.model.fit(X, y, epochs=epochs, batchsize=batchsize, validation_split=validation_split, verbose=verbose)
-
-    def update(self, dataset):
-        states, values, policys  = dataset.get_traindata()
-        self.model.train_on_batch(states, [values, policys])
     
     def _policy_value_block(self,input_tensor):
         out = Conv2D(
@@ -373,6 +255,121 @@ class AlphaZero:
 
         return out
 
+class AI:
+    def __init__(self, state_shape, role, is_selfplay, model=None, verbose=False):
+        self.role = role
+        self.is_selfplay = is_selfplay
+        self.verbose = verbose
+
+        if model is not None:
+            self.model = model
+        else:
+            hidden_layers = list()
+            hidden_layers.append({'nb_filter':20, 'kernel_size':3})
+            hidden_layers.append({'nb_filter':20, 'kernel_size':3})
+            hidden_layers.append({'nb_filter':20, 'kernel_size':3})
+            hidden_layers.append({'nb_filter':20, 'kernel_size':3})
+
+            self.model = NeuralNetwork(
+                input_shape=state_shape, 
+                hidden_layers=hidden_layers
+                )
+            
+            if self.verbose:
+                print("Initiating AI model...",end="")
+            self.model.init()
+            if self.verbose:
+                print("End")
+    
+    def get_model(self):
+        return self.model
+
+    def load_model(self, filename):
+        '''
+        Load Model with file name
+        '''
+        from keras.models import load_model as LOAD_MODEL
+        self.model = LOAD_MODEL(filename)
+
+    def save_model(self, filename):
+        '''
+        Save model with file name
+        '''
+        self.model.save(filename)
+
+    def plot_model(self, filename='model.png'):
+        from keras.utils import plot_model
+        plot_model(self.model, show_shapes=True, to_file=filename)
+
+    def evaluate_function(self, engine, role=None, s_reversal=False):
+        '''
+        Evaluate function
+        '''
+        if role is None:
+            role = self.role
+        state = engine.get_state(role, s_reversal)
+        value, probs = self.model.predict(state)
+
+        actions = engine.get_availables()
+        probs = probs[0][actions]
+        probs = probs/np.sum(probs)
+        action_probs = zip(actions, probs) # actions with their probabilities
+        return value[0][0], action_probs
+
+    def train(self, data, batchsize=128, epochs=30, verbose=False):
+        '''
+        Train AI
+        '''
+        Xs, ys = zip(*data)
+        states = Xs
+        values, policys = zip(*ys) 
+        self.model.fit(states, [values, policys] 
+            epochs=epochs, 
+            batchsize=batchsize, 
+            verbose=verbose)
+
+    def update(self, data):
+        '''
+        Update AI
+        '''
+        Xs, ys = zip(*data)
+        states = Xs
+        values, policys = zip(*ys) 
+        self.model.train_on_batch(states, [values, policys])
+
+    def play(self, engine, temperature=1.0):
+        '''
+        Play game (or self-play)
+        '''
+        n_search = 10
+        c_puct = 0.95
+        n_playout = 10
+
+        if self.is_selfplay:
+            actions = list()
+            for i in range(n_search):  
+                _engine = engine.clone()
+                mcts = MCTS(
+                        evaluate_function=self.evaluate_function, 
+                        c_puct=c_puct, 
+                        n_playout=n_playout, 
+                        verbose=self.verbose
+                    )
+                # MCTS_actions should be 1D vector
+                mcts_actions, probs = self.mcts.search(engine=_engine) 
+                action = np.random.choice(
+                    mcts_actions,
+                    p=0.75*probs + 0.25*np.random.dirichlet(0.3*np.ones(len(probs)))
+                )
+                actions.append(action)
+            action = np.random.choice(action)
+        else:
+            value, action_probs = self.evaluate_function(engine)
+            actions, probs = zip(*action_probs)
+            index = np.argmax(probs)
+            action = actions[index]
+
+        return action
 
 
 if __name__=='__main__':
@@ -388,9 +385,4 @@ if __name__=='__main__':
     from keras.utils import plot_model
     plot_model(alphazero.model,show_shapes=True,show_layer_names=False, to_file='model.png')
     print(alphazero.model.summary())
-
-    # input_data = np.random.random((1,*input_size))
-    # value, action_prob = alphazero.model.predict(input_data)
-    # print(value)
-    # print(action_prob)
         
--- alphagomoku/train.py
+++ alphagomoku/train.py
@@ -1,168 +1,122 @@
-import numpy as np
-from deepai.alphazero import AlphaZero 
-from collections import defaultdict, deque
-from selfplay import SelfplayEngine
-
-#
-# Utils for self-play
-#
 from __future__ import absolute_import
 
 import numpy as np 
-from mcts import MCTSPlayer
 import Config
-import Computer
-from Gameengine import GameEngine
+from gameengine import GameEngine
+from ai import NeuralNetwork, AI
 
 class SelfplayEngine:
-    def __init__(self, ai, c_puct, n_playout, verbose=False):
-        self.ai = ai
-        self.dataset = list()
-        self.gameenigne = None
-        self.c_puct = c_puct
-        self.n_playout = n_playout
+    '''
+    Self-play game engine to get the training data
+    '''
+    def __init__(self, state_shape, model, verbose=False):
+        self.state_shape = state_shape
+        self.model = model
         self.verbose = verbose
 
-    def init(self):
-        self.dataset = list()
-        computerA = MCTSPlayer(value_function=self.ai.value_function, 
-                        c_puct=self.c_puct, 
-                        n_playout=self.n_playout, 
-                        is_selfplay=True,
-                        role='Self_A',
-                        verbose=self.verbose)
-        computerB = MCTSPlayer(value_function=self.ai.value_function, 
-                        c_puct=self.c_puct, 
-                        n_playout=self.n_playout, 
-                        is_selfplay=True,
-                        role='Self_B',
-                        verbose=self.verbose)
-        self.gameenigne = GameEngine(playerA=computerA,playerB=computerB,verbose=self.verbose)
+    def start(self):
+        '''
+        Start a self-play game
+        '''
+        # Self-player A
+        roleA = 'Self_A'
+        playerA = AI(
+            state_shape=self.state_shape, 
+            role=roleA,  # The customed string with specific meaning 
+            is_selfplay=True, 
+            model=self.model, 
+            verbose=self.verbose
+        )
+        # Self-player B
+        roleB = 'Self_B'
+        playerB = AI(
+            state_shape=self.state_shape, 
+            role=roleB,  # The customed string with specific meaning 
+            is_selfplay=True, 
+            model=self.model, 
+            verbose=self.verbose
+        )
 
-    def get_data(self):
+        engine = GameEngine(
+            players=[playerA, playerB], 
+            state_shape=self.state_shape, 
+            is_selfplay=True,
+            verbose=verbose)
+
         if self.verbose:
-            print("Self-playing...")
-        gamedata = self.gameenigne.run()
-        #winner, train_data = gamedata.getalldata()
-        return gamedata
+            print("Start to self-play...")
+        winner = engine.start()
+        if self.verbose:
+            print("End of self-play")
 
-class Evaluator:
-    def __init__(self,ai,verbose=False):
-        self.ai = ai
-        self.evaluate_player = None
+        data = engine.get_data()
+        data_A, data_B = zip(*data)
+        _, ys_A = zip(*data_A)
+        values_A, _ = zip(*ys_A)
+        _, ys_B = zip(*data_B)
+        values_B, _ = zip(*ys_B)
 
-    def evaluate(self):
-        # TODO play with naive MCTS player to evaluate
+        # Not to copy, operate on the reference
+        if winner == roleA:
+            values_A, values_B = 1, -1
+        else:
+            values_A, values_B = -1, 1
 
-        win_ratio = 0
+        return data
 
-        return win_ratio
+class EvaluationEngine:
+    '''
+    Evaluation Engine to evaluate the performance of AI model
+    '''
+    def __init__(self):
+        pass
 
 class TrainAI:
     def __init__(self,
-        board_size=(10,10),
-        learning_rate=2e-3,
-        lr_multiplier=1.0,
-        temperature=1.0,
-        n_playout=400,
-        n_playout_mcts=1000,
-        n_playout_checkpoint=1000,
-        c_puct=5,
-        buffer_size=10000,
-        batch_size=512,
-        selfplay_batch_size=1,
-        epochs=5,
-        kl_targ=0.02,
-        check_freq=50,
-        game_batch_size=1500,
-        best_win_ratio=0.0,
-        parallelize=1,
-        vectorlength=3,
+        state_shape=(10,10,3),
         verbose=False):
 
-        self.game = None # instance of game engine
-        self.board_size = board_size    # size of board
-        self.learning_rate = learning_rate  # learning rate for training network
-        self.lr_multiplier = lr_multiplier  # adjust learning rate adaptively based on KL
-        self.temperature = temperature  # temperature to control anneal
-        self.n_playout = n_playout  # number of simulations for each move
-        self.n_playout_mcts = n_playout_mcts    # num of simulations to evaluate the trained network
-        self.n_playout_checkpoint = n_playout_checkpoint
-        self.c_puct = c_puct    # constant determining the level of exploration
-        self.buffer_size = buffer_size 
-        self.batch_size = batch_size    # batch size for training network
-        self.selfplay_batch_size = selfplay_batch_size # number of self-play simulations
-        self.epochs = epochs    # number of train steps for each update
-        self.kl_targ = kl_targ
-        self.check_freq = check_freq
-        self.game_batch_size = game_batch_size
-        self.best_win_ratio = best_win_ratio
-        self.parallelize = parallelize
+        self.state_shape = state_shape
         self.verbose = verbose
 
-        # TODO initiate alphazero model
         hidden_layers = list()
         hidden_layers.append({'nb_filter':20, 'kernel_size':3})
         hidden_layers.append({'nb_filter':20, 'kernel_size':3})
         hidden_layers.append({'nb_filter':20, 'kernel_size':3})
         hidden_layers.append({'nb_filter':20, 'kernel_size':3})
 
+        self.model = NeuralNetwork(
+                input_shape=state_shape, 
+                hidden_layers=hidden_layers
+                )
+            
+        if self.verbose:
+            print("Initiating AI model...",end="")
+        self.model.init()
+        if self.verbose:
+            print("End")
 
-        input_size = (*board_size, vectorlength)
-        self.ai = AlphaZero(
-            input_size=input_size,
-            hidden_layers=hidden_layers,
-            learning_rate=1e-4,
-            momentum=0.9,
-            l2_const=1e-4,
-            verbose=False
-        )
-
-        self.ai.init()
-
-    def get_ai(self):
-        return self.ai
-
-    def get_data_extended(self,play_data):
-        '''
-        Augment the data set by rotation and flipping according to the symmetry
-        '''
-        extend_data = []
-
-        # TODO achieve rotation and flipping operations
-
-        return extend_data
+    def get_model(self):
+        return self.model   
 
     def get_selfplay_data(self):
         '''
         Get MCTS self-play data for training
         '''
-        dataset = list()
-        for i in range(self.selfplay_batch_size):
-            selfplayengine = SelfplayEngine(
-                ai=self.ai, 
-                c_puct=self.c_puct, 
-                n_playout=self.n_playout, 
-                verbose=self.verbose)
-            selfplayengine.init()
-            dataset.append(selfplayengine.get_data())
-        return dataset
+        selfplayengine = SelfplayEngine(
+                state_shape=self.state_shape, 
+                model=self.model, 
+                verbose=False)
+        data = selfplayengine.start()
+        return data
 
     def network_update(self, dataset):
         '''
         update AlphaZero policy-value network
         '''
-        return 0, 0
-
-        n_dataset = len(dataset)
-        Xs_train, values_train, policy_train = list(), list(), list()
-        for i in range(n_dataset):
-            Xs, values, policy = dataset.get_traindata()
-            Xs_train.append(Xs)
-            values_train.append(values)
-            policy_train.append(policy)
-
-        self.ai.train(Xs_train, [values_train, policy_train])
+        loss, entropy = 0, 0
+        
+        # TODO update Neural network model
 
         return loss, entropy
 
@@ -170,11 +124,11 @@ class TrainAI:
         '''
         get win ratio 
         '''
+        win_ratio = 0
+
+        # TODO evaluate Neural network model
 
         return win_ratio
-
-    def savemodel(self):
-        pass
 
     def start(self):
         '''
@@ -184,35 +138,34 @@ class TrainAI:
             hyper-parameters for MCTS
             3. Return 1 until convergence or satisfying the end conditions
         '''
-        try:
-            for i in range(self.game_batch_size):
-                if self.verbose:
-                    print("Train Batch {0}: ".format(i+1))
-                play_data = self.get_selfplay_data()
+        epochs = 1000
+        check_freq = 50
+
+        for i in range(epochs):
+            if self.verbose:
+                print("Train Batch {0}: ".format(i+1))
+            play_data = self.get_selfplay_data()
             
-                if self.verbose:
-                    print("Updating network...",end="")
-                loss, entropy = self.network_update(play_data)
-                if self.verbose:
-                    print("End with loss = {loss}, entropy = {entropy}".format(
-                        loss=loss,
-                        entropy=entropy
-                    ))
+            if self.verbose:
+                print("Updating network...",end="")
+            loss, entropy = self.network_update(play_data)
+            if self.verbose:
+                print("End with loss = {loss}, entropy = {entropy}".format(
+                    loss=loss,
+                    entropy=entropy
+                ))
 
-                if (i+1) % self.check_freq == 0:
-                    if self.verbose:
-                        print("Checkpoint: Evaluating...",end="")
-                    win_ratio = self.network_evaluate()
-                    if self.verbose:
-                        print("Saving model...",end="")
-                    self.ai.save("./model.h5")
-                    if win_ratio > self.best_win_ratio:
-                        print("New best AI...",end="")
-                        self.best_win_ratio = win_ratio
-                        self.ai.save("./best_model.h5")
-                        if (self.best_win_ratio == 1.0 and self.n_playout_checkpoint < 5000):
-                            self.n_playout_checkpoint += 1000
-                            self.best_win_ratio = 0.0
-
-        except KeyboardInterrupt:
-            print('\n\r Train Quit by user manually')
+            if (i+1) % check_freq == 0:
+                if self.verbose:
+                    print("Checkpoint: Evaluating...",end="")
+                win_ratio = self.network_evaluate()
+                if self.verbose:
+                    print("Saving model...",end="")
+                self.ai.save("./model.h5")
+                if win_ratio > self.best_win_ratio:
+                    print("New best AI...",end="")
+                    self.best_win_ratio = win_ratio
+                    self.ai.save("./best_model.h5")
+                    if (self.best_win_ratio == 1.0 and self.n_playout_checkpoint < 5000):
+                        self.n_playout_checkpoint += 1000
+                        self.best_win_ratio = 0.0
